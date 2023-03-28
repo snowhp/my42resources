62c672f5c2d8bafc09436c4fa91d79195a38a1a9
SnowHP <tiagojsmachado@hotmail.com>
2023-03-28 15:38:50 +0100

    norm fixed

--- libft/ft_joinsplit.c
+++ libft/ft_joinsplit.c
@@ -6,67 +6,70 @@
 /*   By: tde-sous <tde-sous@42.porto.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 09:16:26 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/03/28 14:28:55 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/03/28 15:38:33 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /* Count numbers of words separated by 'c'.*/
-static int	ft_wcount(const char *str, char c, char cj)
+void	ft_wcount(const char *str, char c, char cj, int *wcount)
 {
 	int	i;
 	int	ctrl;
 	int	ctrl1;
-	int	wcount;
 
-	wcount = 0;
 	ctrl = 0;
 	ctrl1 = 0;
 	i = 0;
-	while (str[i] == ' ' || str[i] == '\t')
-		i++;
 	while (str[i] != '\0')
 	{
 		if (!ctrl1 && str[i] == cj)
 		{
 			ctrl1 = 1;
-			wcount++;
+			(*wcount)++;
 		}
 		else if (ctrl1 && str[i] == cj)
 			ctrl1 = 0;
 		if (!ctrl && !ctrl1 && str[i] != c && str[i] != cj)
 		{
 			ctrl = 1;
-			wcount++;
+			(*wcount)++;
 		}
 		else if (ctrl && str[i] == c)
 			ctrl = 0;
 		i++;
 	}
-	return (wcount);
 }
 
-static char	*ft_word(const char *str, char c)
+int	ft_checkchar(const char *str, char c)
 {
-	int		l;
-	int		i;
 	int		ctrl;
-	char	*res;
+	int		l;
 
 	l = 0;
 	ctrl = 0;
 	while (str[l] != '\0' && str[l] != c)
 	{
-		if(str[l] == 39 && !ctrl)
+		if (str[l] == 39 && !ctrl)
 		{
 			ctrl = 1;
 			c = '\'';
 		}
 		l++;
 	}
-	if(c == '\'')
+	if (c == '\'')
 		l++;
+	return (l);
+}
+
+static char	*ft_word(const char *str, char c)
+{
+	int		l;
+	int		i;
+	char	*res;
+
+	l = ft_checkchar(str, c);
 	res = (char *)malloc(sizeof(char) * (l + 1));
 	if (!res)
 		return (NULL);
@@ -77,18 +80,20 @@ static char	*ft_word(const char *str, char c)
 	return (res);
 }
 
-/* This function will split a string by give char c, but joining whats inside cj,
-if its a even number otherwise return 0*/
+/* This function will split a string by give char c, but joining whats inside 
+cj, if its a even number otherwise return 0*/
 char	**ft_joinsplit(char const *s, char c, char cj)
 {
 	int		wcount;
 	int		i;
 	char	**result;
 
-	wcount = ft_wcount(s, c, cj);
-	i = 0;
-	if((ft_countchars(s, cj) % 2) != 0)
-		return(0);
+	wcount = 0;
+	while (*s == ' ' || *s == '\t')
+		s++;
+	ft_wcount(s, c, cj, &wcount);
+	if ((ft_countchars(s, cj) % 2) != 0)
+		return (0);
 	result = (char **)malloc((wcount + 1) * sizeof(char *));
 	if (!result)
 		return (NULL);
@@ -97,17 +102,16 @@ char	**ft_joinsplit(char const *s, char c, char cj)
 	{
 		while (*s == c)
 			s++;
-		if(*s == cj)
+		if (*s == cj)
 			result[i] = ft_word(s, c);
 		else
 			result[i] = ft_word(s, c);
 		s += ft_strlen(result[i++]);
 	}
-	result[i] = NULL;
 	return (result);
 }
 
-#include <stdio.h>
+/* #include <stdio.h>
 int	main()
 {
 	char	*s;
@@ -127,3 +131,4 @@ int	main()
 	}
 	return (0);
 }
+ */
